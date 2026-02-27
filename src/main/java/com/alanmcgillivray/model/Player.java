71d635bfd6ccb53b563c185ec14b57b6560d92f9
Alan <alan.mcgillivray@griffith.student.ie>
2026-02-27 13:32:19 +0000

    Added player entity class

---
+++ src/main/java/com/alanmcgillivray/model/Player.java
@@ -0,0 +1,25 @@
+package com.alanmcgillivray.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int level, health, strength, defense, speed;
+    
+
+}
